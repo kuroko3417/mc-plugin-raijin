fc2eb97f14639cedb67e3b2b41b74330952862d3
kuroko3417 <kuroko3417@gmail.com>
2020-10-01 01:35:05 +0900

    - 各バージョンのプロジェクト追加 - 開発用コンテナの設定ファイル追加

---
+++ Raijin-1.16.1/src/main/java/kuroko3417/raijin/Raijin.java
@@ -0,0 +1,17 @@
+package kuroko3417.raijin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Raijin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
